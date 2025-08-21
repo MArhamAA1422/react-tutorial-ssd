b16e461417af21467422b50650e69609bde157aa
Md::Arham <arham511624@gmail.com>
2025-08-21 13:19:38 +0600

    Logo Icon reformation

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import { Header } from '../components/Header';
 import './HomePage.css';
+import CheckmarkIcon from '../assets/icons/checkmark.png';
+
 export function HomePage() {
   return (
     <>
       <title>Ecommerce Project</title>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
       <Header />
       <div className="home-page">
         <div className="products-grid">
@@ -47,7 +50,7 @@ export function HomePage() {
             <div className="product-spacer"></div>
 
             <div className="added-to-cart">
-              <img src="images/icons/checkmark.png" />
+              <img src={CheckmarkIcon} />
               Added
             </div>
 
@@ -96,7 +99,7 @@ export function HomePage() {
             <div className="product-spacer"></div>
 
             <div className="added-to-cart">
-              <img src="images/icons/checkmark.png" />
+              <img src={CheckmarkIcon} />
               Added
             </div>
 
@@ -145,7 +148,7 @@ export function HomePage() {
             <div className="product-spacer"></div>
 
             <div className="added-to-cart">
-              <img src="images/icons/checkmark.png" />
+              <img src={CheckmarkIcon} />
               Added
             </div>
 
